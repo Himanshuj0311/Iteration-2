13a549b055b9e41cd93aace6698b241e8f2cd18e
Himanshu Jain <112754762+Himanshuj0311@users.noreply.github.com>
2025-06-26 21:02:25 +0530

    Update thisKeyword.js

--- thisKeyword.js
+++ thisKeyword.js
@@ -8,3 +8,30 @@ let student = {
 let jsonString = JSON.stringify(student);
 
 console.log(jsonString)
+
+
+//synchronous code execution
+console.log("Task 1");
+console.log("Task 2");
+console.log("Task 3");
+
+
+console.log("Message 1")
+const setime=setTimeout(()=>{
+  console.log("Message after 2 second");
+},2000)
+
+console.log("Message 3")
+
+let count = 0;
+
+// Start the loading interval
+let loadingInterval = setInterval(() => {
+  console.log("Loading...");
+  count++;
+
+  if (count === 5) {
+    clearInterval(loadingInterval); 
+    console.log("Loaded successfully!");
+  }
+}, 1000); 
